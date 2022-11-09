358dd73dbe79e8e2b29efa95948ced465f8f3358
EdgarHernandez13 <alex.hernandez@sryas.com>
2022-11-09 16:52:06 -0600

    adding buttoms to js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // We will call pokemon API.
-console.log("I am calling pokemon API.")
-
 let pokemon = null;
-
 const pokemonName = document.getElementById("pokemon-name")
 const pokemonImage = document.getElementById("pokemon-image")
 const pokemonInfo = document.getElementById("pokemon-info")
 let id = null
 const searchBox = document.getElementById("searchBox")
 const button = document.getElementById("submit")
+
+id=1
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`,{
+    method: "GET"
+}).then(res=>res.json().then(data => {
+    pokemon = data
+    pokemonImage.setAttribute("src", pokemon.sprites.front_default)
+    pokemonName.innerText = pokemon.name
+    pokemonInfo.innerHTML = `<span id="height"> Height: ${pokemon.height} </span> <span id="weight"> Weight: ${pokemon.weight} </span>`
+}))
 
 button.addEventListener("click", (e)=>{
     id=searchBox.value
